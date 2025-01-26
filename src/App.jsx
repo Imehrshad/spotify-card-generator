1fee58db92f7fcb6094c15d2208ee8f44c23aa34
Mehrshad Khatibi <khatibimehrshad24@gmail.com>
2025-01-26 22:15:07 +0330

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,100 @@
+import { useRef, useState } from "react";
+import Card from "./components/Card";
+import ResultImage from "./components/ResultImage";
+
+function App() {
+  const [values, setValues] = useState({
+    song: "",
+    artist: "",
+    lyrics: "",
+    musicCover: null,
+    backgroundColor: "",
+    logoColor: "",
+  });
+  const cardRef = useRef(null);
+  const [clickCounter, setClickCounter] = useState(0);
+
+  return (
+    <>
+      <div className="w-full  bg-black text-white ">
+        <div className="w-full h-full flex flex-col items-center justify-start py-5 px-5 gap-2">
+          <Card values={values} clickCounter={clickCounter}    cardRef={cardRef}/>
+          <div className="w-full h-full bg-zinc-500 rounded-lg flex flex-col items-center justify-start py-5 px-5 gap-3">
+            <h1 className="text-xl font-bold">Creating a new card</h1>
+            <label htmlFor="card-name" className="text-sm">
+              Song
+            </label>
+            <input
+              value={values.song}
+              onChange={(e) => setValues({ ...values, song: e.target.value })}
+              type="text"
+              className="w-full py-1  rounded-lg focus:outline-none border-zinc-800 border-2  px-3 bg-zinc-400"
+            />
+            <label htmlFor="card-name" className="text-sm">
+              Artist
+            </label>
+            <input
+              value={values.artist}
+              onChange={(e) => setValues({ ...values, artist: e.target.value })}
+              type="text"
+              className="w-full py-1  rounded-lg focus:outline-none border-zinc-800 border-2  px-3 bg-zinc-400"
+            />
+            <label htmlFor="lyrics" className="text-sm">
+              Lyrics
+            </label>
+            <textarea
+              id="lyrics"
+              name="lyrics"
+              style={{ resize: "none" }}
+              value={values.lyrics}
+              onChange={(e) => setValues({ ...values, lyrics: e.target.value })}
+              className="w-full h-24 py-1 rounded-lg focus:outline-none border-zinc-800 border-2 px-3 bg-zinc-400"
+            />
+            <label htmlFor="card-name" className="text-sm">
+              Music cover
+            </label>
+            <input
+              onChange={(e) =>
+                setValues({ ...values, musicCover: e.target.files[0] })
+              }
+              type="file"
+              accept="image/*"
+              className="w-full py-4 rounded-lg focus:outline-none border-zinc-800 border-2 px-3 bg-zinc-400"
+            />
+            <label htmlFor="background-color" className="text-sm">
+              Background Color
+            </label>
+            <input
+              type="color"
+              className="w-1/2 h-10 rounded-lg focus:outline-none border-zinc-800 border-2  px-3 bg-zinc-400"
+              value={values.backgroundColor}
+              onChange={(e) =>
+                setValues({ ...values, backgroundColor: e.target.value })
+              }
+            />
+            <label htmlFor="logo-color" className="text-sm">
+              Logo Color
+            </label>
+            <input
+              type="color"
+              className="w-1/2 h-10 rounded-lg focus:outline-none border-zinc-800 border-2  px-3 bg-zinc-400"
+              value={values.logoColor}
+              onChange={(e) =>
+                setValues({ ...values, logoColor: e.target.value })
+              }
+            />
+            <button
+              className="w-full py-2 rounded-lg bg-green-500 text-white hover:bg-green-600 cursor-pointer"
+              onClick={() => setClickCounter((prev) => prev + 1)}
+            >
+              Download
+            </button>
+          </div>
+        </div>
+      </div>
+
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Card.jsx
@@ -0,0 +1,73 @@
+import React, { useEffect, useState, useRef } from "react";
+import SpotifySvg from "./SpotifySvg";
+import html2canvas from "html2canvas";
+
+const Card = ({ values, clickCounter, cardRef }) => {
+  const { song, artist, lyrics, musicCover, logoColor, backgroundColor } =
+    values;
+  const [imageUrl, setImageUrl] = useState(null);
+  const downloadAsImage = async () => {
+    if (cardRef.current) {
+      try {
+        const canvas = await html2canvas(cardRef.current, {
+          backgroundColor: null,
+          scale: 2, // Higher quality
+        });
+
+        const image = canvas.toDataURL("image/png", 1.0);
+        const link = document.createElement("a");
+        link.download = `${song || "music"}-card.png`;
+        link.href = image;
+        link.click();
+      } catch (err) {
+        console.error("Error generating image:", err);
+      }
+    }
+  };
+  useEffect(() => {
+    if (clickCounter > 0) {
+      downloadAsImage();
+    }
+  }, [clickCounter]);
+
+  useEffect(() => {
+    if (musicCover && musicCover instanceof File) {
+      const url = URL.createObjectURL(musicCover);
+      setImageUrl(url);
+
+      // Cleanup the URL when component unmounts or musicCover changes
+      return () => URL.revokeObjectURL(url);
+    }
+  }, [musicCover]);
+
+  return (
+    <>
+      <div
+        ref={cardRef}
+        className="flex flex-col items-center justify-center w-full gap-2 rounded-lg py-7 px-7"
+        style={{ backgroundColor: backgroundColor }}
+      >
+        <div className="flex items-center justify-start gap-5 w-full">
+          <img
+            src={imageUrl || "default-image-path.jpg"}
+            alt="album cover"
+            className="w-24 h-24"
+          />
+          <div>
+            <h3 className="text-xl font-bold">{song || "music title"}</h3>
+            <h4 className="text-sm">{artist || "artist name"}</h4>
+          </div>
+        </div>
+        <div className="w-full h-full dual-font">
+          <p className="text-sm">{lyrics || "lorem ipsum dolor sit amet"}</p>
+        </div>
+        <SpotifySvg
+          className="w-16    h-16   self-start text-start p-0"
+          fill={logoColor || "#FFFF"}
+        />
+      </div>
+    </>
+  );
+};
+
+export default Card;
